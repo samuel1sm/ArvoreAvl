f242ed7a6bd0e08bb31990f7a0b000e1f5c7d5f4
samuel1sm <fsamuelsmartins@gmail.com>
2018-05-16 22:57:26 -0300

    avl sem avl 2

--- AvlTree/include/Node.hpp
+++ AvlTree/include/Node.hpp
@@ -1,11 +1,11 @@
 #ifndef NODE_H
 #define NODE_H
 
-
+using namespace std;
 class Node
 {
     public:
-        Node(int d) {
+        Node(string d) {
         left=NULL;
         right=NULL;
         dado=d;
@@ -13,17 +13,17 @@ class Node
 
         Node* getRight(){return right;}
         Node* getLeft(){return left;}
-        int getDado(){return dado;}
+        string getDado(){return dado;}
         void setRight(Node* no){right=no;}
         void setLeft(Node* no){left=no;}
-        void setDado(int d){dado=d;}
+        void setDado(string d){dado=d;}
         void setHeight(int d){height =d;}
 
     private:
         Node* left;
         Node* right;
         int height;
-        int dado;
+        string dado;
 };
 
 #endif // NODE_H
